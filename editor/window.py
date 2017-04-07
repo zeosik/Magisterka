89570875357f3ace17bac3515f04b0f73d809ad8
Marek <marekc.work@gmail.com>
2017-04-07 21:19:39 +0200

    prosty widget z wyswietlaniem grafu graczy

--- editor/window.py
+++ editor/window.py
@@ -3,6 +3,8 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gio, Gtk
 
+from editor.playerswidget import *
+
 
 MENU_XML="""
 <?xml version="1.0" encoding="UTF-8"?>
@@ -24,6 +26,13 @@ class EditorWindow(Gtk.ApplicationWindow):
     def __init__(self, app):
         Gtk.ApplicationWindow.__init__(self, title="Editor", application=app)
         self.set_position(Gtk.WindowPosition.CENTER)
+        self.players_widget = PlayersWidget()
+
+        #przykladowe dane
+        self.players_widget.add_player(Player("Super"))
+        self.players_widget.add_player(Player("Super2"))
+
+        self.add(self.players_widget)
 
 
 class EditorApplication(Gtk.Application):
