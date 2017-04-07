1dc197c734c91da72c9d1f4216beec8b22255721
Marek <marekc.work@gmail.com>
2017-04-07 17:31:57 +0200

    zamiana na Application/ApplicationWindow

--- editor/window.py
+++ editor/window.py
@@ -1,18 +1,54 @@
+import sys
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gio, Gtk
 
 
-class EditorWindow(Gtk.Window):
-    def __init__(self, geometry = (400, 500)):
-        Gtk.Window.__init__(self, title="Editor")
+MENU_XML="""
+<?xml version="1.0" encoding="UTF-8"?>
+<interface>
+    <menu id="app-menu">
+        <section>
+            <item>
+                <attribute name="action">app.quit</attribute>
+                <attribute name="label" translatable="yes">_Quit</attribute>
+                <attribute name="accel">&lt;Primary&gt;q</attribute>
+            </item>
+        </section>
+    </menu>
+</interface>
+"""
 
-        self.set_default_size(geometry[0], geometry[1])
 
-        self.connect("delete-event", Gtk.main_quit)
+class EditorWindow(Gtk.ApplicationWindow):
+    def __init__(self, app):
+        Gtk.ApplicationWindow.__init__(self, title="Editor", application=app)
+        self.set_position(Gtk.WindowPosition.CENTER)
+
+
+class EditorApplication(Gtk.Application):
+    def __init__(self):
+        Gtk.Application.__init__(self)
+
+    def do_activate(self):
+        win = EditorWindow(self)
+        win.show_all()
+
+    def do_startup(self):
+        Gtk.Application.do_startup(self)
+
+        action = Gio.SimpleAction.new("quit", None)
+        action.connect("activate", self.on_quit)
+        self.add_action(action)
+
+        builder = Gtk.Builder.new_from_string(MENU_XML, -1)
+        self.set_app_menu(builder.get_object("app-menu"))
+
+    def on_quit(self, action, param):
+        self.quit()
 
 
 def run():
-    editor = EditorWindow()
-    editor.show_all()
-    Gtk.main()
+    app = EditorApplication()
+    exit_status = app.run(sys.argv)
+    sys.exit(exit_status)
