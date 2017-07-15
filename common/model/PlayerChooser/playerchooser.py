0268dbec489ba19d4fe329306b47f6958b67bc49
Marek <marekc.work@gmail.com>
2017-07-15 17:52:33 +0200

    todo

--- common/model/PlayerChooser/playerchooser.py
+++ common/model/PlayerChooser/playerchooser.py
@@ -9,6 +9,7 @@ class PlayerChooser(PlayerInput):
         super().__init__(name)
         self.name = name
 
+    #TODO przejsc na metode z playerinput ale to sam moge zrobic
     def player(self, gamestate: GameState) -> Player:
         pass
 
